e67ef448914d5a60932aa0c6b34002adf4e6c617
Tumandang <danishdanial0991@gmail.com>
2026-01-28 11:28:14 +0800

    kalike18

--- config/filesystems.php
+++ config/filesystems.php
@@ -49,8 +49,8 @@ return [
         'cloudinary' => [
             'driver' => 'cloudinary',
             'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-            'api_key' => env('CLOUDINARY_API_KEY'),
-            'api_secret' => env('CLOUDINARY_API_SECRET'),
+            'api_key' => env('CLOUDINARY_KEY'),
+            'api_secret' => env('CLOUDINARY_SECRET'),
             'url' => [
                 'secure' => true 
             ],
