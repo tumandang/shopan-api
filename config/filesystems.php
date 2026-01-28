47dd27b9e4e68f59d3e16c8e277474892904bc0c
Tumandang <danishdanial0991@gmail.com>
2026-01-28 10:09:48 +0800

    kali-ke12

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,17 +46,6 @@ return [
             'throw' => false,
             'report' => false,
         ],
-
-        'cloudinary' => [
-            'driver' => 'cloudinary',
-            'key' => env('CLOUDINARY_KEY'),
-            'secret' => env('CLOUDINARY_SECRET'),
-            'cloud' => env('CLOUDINARY_CLOUD_NAME'),
-            'url' => env('CLOUDINARY_URL'),
-            'secure' => (bool) env('CLOUDINARY_SECURE', true),
-            'prefix' => env('CLOUDINARY_PREFIX'),
-        ],
-
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
